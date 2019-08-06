af9626271c2dc99b31ff1c97568a1ce1e76368b9
galaprogastyle <53810276+galaprogastyle@users.noreply.github.com>
2019-08-06 10:31:13 +0300

    Add files via upload

---
+++ precache-manifest.ae3c8dbfadc1185c1dc671486c4dfc5f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/static/media/persik.4e1ec840.png"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "041553a46eaa8093c2ca",
+    "url": "/static/js/main.f37f277b.chunk.js"
+  },
+  {
+    "revision": "a49e750a18d658fd502e",
+    "url": "/static/js/2.b27f1798.chunk.js"
+  },
+  {
+    "revision": "041553a46eaa8093c2ca",
+    "url": "/static/css/main.8ee84afd.chunk.css"
+  },
+  {
+    "revision": "a49e750a18d658fd502e",
+    "url": "/static/css/2.e57ba6f8.chunk.css"
+  },
+  {
+    "revision": "a7cc7a68d0d7da8d4c93ea6db2c0b913",
+    "url": "/index.html"
+  }
+];
